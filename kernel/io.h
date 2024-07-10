470152a43f46f686e794243dd896568679c32d0d
Goldside543 <goldside000@outlook.com>
2024-07-10 09:24:14 -0400

    Create io.h

---
+++ kernel/io.h
@@ -0,0 +1,18 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdint.h>
+
+// Function to read a byte from an I/O port
+static inline uint8_t inb(uint16_t port) {
+    uint8_t result;
+    __asm__ __volatile__ ("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+// Function to write a byte to an I/O port
+static inline void outb(uint16_t port, uint8_t data) {
+    __asm__ __volatile__ ("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+#endif // IO_H
