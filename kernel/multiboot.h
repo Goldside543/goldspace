4c0ed2528ece9101dbc38abcb632bb7daa11e450
Goldside543 <goldside000@outlook.com>
2024-07-10 11:22:46 -0400

    Update multiboot.h

--- kernel/multiboot.h
+++ kernel/multiboot.h
@@ -1,19 +1,18 @@
+// kernel/multiboot.h
 #ifndef MULTIBOOT_H
 #define MULTIBOOT_H
 
 #include <stdint.h>
 
-struct multiboot_header {
-    uint32_t magic;
-    uint32_t flags;
-    uint32_t checksum;
-    uint32_t header_addr;
-    uint32_t load_addr;
-    uint32_t load_end_addr;
-    uint32_t bss_end_addr;
-    uint32_t entry_addr;
-};
+typedef struct multiboot_header {
+    uint32_t magic;          // Must be 0x1BADB002
+    uint32_t flags;          // Requested features
+    uint32_t checksum;       // Verification checksum
+    uint32_t header_addr;    // Virtual address of header
+    uint32_t load_addr;      // Virtual address kernel should be loaded at
+    uint32_t load_end_addr;  // Virtual address of end of kernel
+    uint32_t bss_end_addr;   // Virtual address of end of the BSS
+    uint32_t entry_addr;     // Entry point of the kernel
+} multiboot_header_t;
 
-extern void kernel_main();
-
-#endif // MULTIBOOT_H
+#endif /* MULTIBOOT_H */
