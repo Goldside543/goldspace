cce111d65f35396b576c4e2c64079c0427507d84
Goldside543 <goldside000@outlook.com>
2024-07-10 10:00:58 -0400

    Update string.h

--- kernel/string.h
+++ kernel/string.h
@@ -3,9 +3,9 @@
 
 #include <stddef.h>
 
-char *strtok(char *str, const char *delim);
-int strcmp(const char *s1, const char *s2);
-size_t strspn(const char *s, const char *accept);
-char *strpbrk(const char *s, const char *accept);
+char *my_strtok(char *str, const char *delim);
+int my_strcmp(const char *s1, const char *s2);
+size_t my_strspn(const char *s, const char *accept);
+char *my_strpbrk(const char *s, const char *accept);
 
 #endif // STRING_H
