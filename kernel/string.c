89a8fbc83d4e03153c2f31506f8d33bf94d478d7
Goldside543 <goldside000@outlook.com>
2024-07-10 10:00:30 -0400

    Update string.c

--- kernel/string.c
+++ kernel/string.c
@@ -1,34 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-char *strtok(char *str, const char *delim) {
-    static char *last;
-    if (str == NULL) str = last;
-    if (str == NULL) return NULL;
-
-    char *end = str + strspn(str, delim);
-    if (*end == '\0') return NULL;
-
-    char *start = end;
-    end = strpbrk(start, delim);
-    if (end) {
-        *end = '\0';
-        last = end + 1;
-    } else {
-        last = NULL;
-    }
-    return start;
-}
-
-int strcmp(const char *s1, const char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
-}
-
-size_t strspn(const char *s, const char *accept) {
+size_t my_strspn(const char *s, const char *accept) {
     const char *p;
     const char *a;
     size_t count = 0;
@@ -47,7 +20,7 @@ size_t strspn(const char *s, const char *accept) {
     return count;
 }
 
-char *strpbrk(const char *s, const char *accept) {
+char *my_strpbrk(const char *s, const char *accept) {
     const char *a;
     for (; *s != '\0'; ++s) {
         for (a = accept; *a != '\0'; ++a) {
@@ -57,4 +30,31 @@ char *strpbrk(const char *s, const char *accept) {
         }
     }
     return NULL;
+}
+
+char *my_strtok(char *str, const char *delim) {
+    static char *last;
+    if (str == NULL) str = last;
+    if (str == NULL) return NULL;
+
+    char *end = str + my_strspn(str, delim);
+    if (*end == '\0') return NULL;
+
+    char *start = end;
+    end = my_strpbrk(start, delim);
+    if (end) {
+        *end = '\0';
+        last = end + 1;
+    } else {
+        last = NULL;
+    }
+    return start;
+}
+
+int my_strcmp(const char *s1, const char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
