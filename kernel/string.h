c2ffa9613ed4b8bebc5733629c0f8a7e040b1cbe
Goldside543 <goldside000@outlook.com>
2024-07-11 18:06:57 -0400

    Update string.h

--- kernel/string.h
+++ kernel/string.h
@@ -7,5 +7,6 @@ char *my_strtok(char *str, const char *delim);
 int my_strcmp(const char *s1, const char *s2);
 size_t my_strspn(const char *s, const char *accept);
 char *my_strpbrk(const char *s, const char *accept);
+size_t my_strlen(const char *str);
 
 #endif // STRING_H
