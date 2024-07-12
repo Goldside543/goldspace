7f6db7e3dbbfbd16ea4a91826281af5b6941c819
Goldside543 <goldside000@outlook.com>
2024-07-12 10:31:16 -0400

    stole code >:)

--- kernel/string.c
+++ kernel/string.c
@@ -1,72 +1,76 @@
 #include <stddef.h>
 #include <stdint.h>
 
-size_t my_strlen(const char *str) {
-    const char *s;
-    for (s = str; *s; ++s);
-    return s - str;
+short my_strlen(const char *__s){
+    short i = 0;
+    while(*__s){
+        i++;
+        __s++;
+    }
+    return i;
 }
 
-size_t my_strspn(const char *s, const char *accept) {
-    const char *p;
-    const char *a;
-    size_t count = 0;
-
-    for (p = s; *p != '\0'; ++p) {
-        for (a = accept; *a != '\0'; ++a) {
-            if (*p == *a) {
-                ++count;
-                break;
-            }
-        }
-        if (*a == '\0') {
-            return count;
-        }
-    }
-    return count;
+int my_strcmp(const char* s1, const char* s2)
+{
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(const unsigned char*)s1-*(const unsigned char*)s2;
 }
 
-char *my_strpbrk(const char *s, const char *accept) {
-    const char *a;
-    for (; *s != '\0'; ++s) {
-        for (a = accept; *a != '\0'; ++a) {
-            if (*s == *a) {
-                return (char *)s;
-            }
-        }
-    }
-    return NULL;
+char *my_strcat(char *dest, const char *src)
+{
+    char *ret = dest;
+    while (*dest)
+        dest++;
+    while (*dest++ = *src++);
+    return ret;
 }
 
-char *my_strtok(char *str, const char *delim) {
-    static char *last = NULL;
-    if (str != NULL) {
-        last = str;
-    }
-    if (last == NULL) {
-        return NULL;
-    }
-
-    char *start = last + my_strspn(last, delim);
-    if (*start == '\0') {
-        last = NULL;
-        return NULL;
-    }
-
-    char *end = my_strpbrk(start, delim);
-    if (end) {
-        *end = '\0';
-        last = end + 1;
-    } else {
-        last = start + my_strlen(start);
-    }
-    return start;
+char *my_strchr(const char *s, int c)
+{
+    while (*s != (char)c)
+        if (!*s++)
+            return 0;
+    return (char *)s;
 }
 
-int my_strcmp(const char *s1, const char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+char *my_strcpy(char *dest, const char* src)
+{
+    char *ret = dest;
+    while (*dest++ = *src++);
+    return ret;
+}
+
+short my_strspn(const char *s1, const char *s2)
+{
+    short ret=0;
+    while(*s1 && strchr(s2,*s1++))
+        ret++;
+    return ret;    
+}
+
+char *my_strtok(char * str, const char * delim)
+{
+    static char* p=0;
+    if(str)
+        p=str;
+    else if(!p)
+        return 0;
+    str=p+strspn(p,delim);
+    p=str+strcspn(str,delim);
+    if(p==str)
+        return p=0;
+    p = *p ? *p=0,p+1 : 0;
+    return str;
+}
+
+short my_strcspn(const char *s1, const char *s2)
+{
+    short ret=0;
+    while(*s1)
+        if(strchr(s2,*s1))
+            return ret;
+        else
+            s1++,ret++;
+    return ret;
 }
