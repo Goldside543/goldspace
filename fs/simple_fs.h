934ed4a234d338bcf3371c5d2c134d6cda4cf3cf
Goldside543 <goldside000@outlook.com>
2024-07-15 11:54:51 -0400

    Update simple_fs.h

--- fs/simple_fs.h
+++ fs/simple_fs.h
@@ -17,8 +17,16 @@ typedef struct {
 typedef struct {
     File files[MAX_FILES];
     int free_blocks[NUM_BLOCKS];
+    char storage[NUM_BLOCKS * BLOCK_SIZE]; // Simulated storage
 } FileSystem;
 
-FileSystem fs;
+extern FileSystem fs;
+
+// Function declarations
+void fs_init();
+int create_file(const char* name);
+int write_file(int file_index, const char* data, int size);
+int read_file(int file_index, char* buffer, int size);
+int delete_file(int file_index);
 
 #endif // SIMPLE_FS_H
