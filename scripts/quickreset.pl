2f60fcc3c1bef6dbe90d570a04075c62f42cecb5
Goldside543 <goldside000@outlook.com>
2024-07-15 17:09:44 -0400

    Create quickreset.pl

---
+++ scripts/quickreset.pl
@@ -0,0 +1,17 @@
+# Welcome! This script exits the goldspace directory, deletes it, clones it back, and goes into it, resetting your environment to the stable branch!
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Change directory to the parent directory
+chdir '..' or die "Cannot change directory to parent: $!";
+
+# Remove the 'goldspace' directory recursively
+system('rm -r goldspace') == 0 or die "Failed to remove 'goldspace': $!";
+
+# Clone the GitHub repository
+system('gh repo clone Goldside543/goldspace') == 0 or die "Failed to clone repository: $!";
+
+# Change directory to the 'goldspace' directory
+chdir 'goldspace' or die "Cannot change directory to 'goldspace': $!";
