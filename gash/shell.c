f3356abfa55326c40dd1124de52e9346f6f495a4
Goldside543 <goldside000@outlook.com>
2024-07-16 12:34:43 -0400

    Update shell.c

--- gash/shell.c
+++ gash/shell.c
@@ -7,22 +7,21 @@
 #include "../fs/simple_fs.h"
 
 // Function prototypes for file system shell commands
-void shell_create_file(const char *name);
-void shell_write_file(const char *name, const char *data);
-void shell_read_file(const char *name);
-void shell_delete_file(const char *name);
+void shell_create(const char *args);
+void shell_write(const char *args);
+void shell_read(const char *args);
+void shell_delete(const char *args);
 
 void print(const char *str);
 
 void shell_help() {
     print("Available commands:\n");
-    print("help - Display this help message\n");
-    print("echo <message> - Echo the message back to the screen\n");
-    print("clear - Clear the console screen\n");
-    print("create <filename> - Create a new file\n");
-    print("write <filename> <data> - Write data to a file\n");
-    print("read <filename> - Read data from a file\n");
-    print("delete <filename> - Delete a file\n");
+    print("h - Display this help message\n");
+    print("e <message> - Echo the message back to the screen\n");
+    print("c <filename> - Create a new file\n");
+    print("w <filename> <data> - Write data to a file\n");
+    print("r <filename> - Read data from a file\n");
+    print("d <filename> - Delete a file\n");
 }
 
 void shell_echo(const char *message) {
@@ -37,7 +36,7 @@ void shell_clear() {
     }
 }
 
-void shell_create_file(const char *name) {
+void shell_create(const char *name) {
     int result = create_file(name);
     if (result >= 0) {
         print("File created successfully.\n");
@@ -46,72 +45,46 @@ void shell_create_file(const char *name) {
     }
 }
 
-void shell_write_file(const char *name, const char *data) {
-    int file_index = -1;
-    for (int i = 0; i < MAX_FILES; i++) {
-        if (my_strcmp(fs.files[i].name, name) == 0) {
-            file_index = i;
-            break;
+void shell_write(const char *args) {
+    char *filename = my_strtok((char *)args, " ");
+    char *data = my_strtok(NULL, "");
+    if (filename != NULL && data != NULL) {
+        int file_index = find_file_index(filename);
+        if (file_index != -1) {
+            int result = write_file(file_index, data, my_strlen(data));
+            if (result == 0) {
+                print("Data written successfully.\n");
+                return;
+            }
         }
     }
-
-    if (file_index == -1) {
-        print("Error: File not found.\n");
-        return;
-    }
-
-    int result = write_file(file_index, data, my_strlen(data));
-    if (result == 0) {
-        print("Data written successfully.\n");
-    } else {
-        print("Error: Could not write data to file.\n");
-    }
+    print("Error: Could not write data to file.\n");
 }
 
-void shell_read_file(const char *name) {
-    int file_index = -1;
-    for (int i = 0; i < MAX_FILES; i++) {
-        if (my_strcmp(fs.files[i].name, name) == 0) {
-            file_index = i;
-            break;
+void shell_read(const char *name) {
+    int file_index = find_file_index(name);
+    if (file_index != -1) {
+        char buffer[BLOCK_SIZE];
+        int result = read_file(file_index, buffer, BLOCK_SIZE);
+        if (result == 0) {
+            print(buffer);
+            print("\n");
+            return;
         }
     }
-
-    if (file_index == -1) {
-        print("Error: File not found.\n");
-        return;
-    }
-
-    char buffer[BLOCK_SIZE];
-    int result = read_file(file_index, buffer, BLOCK_SIZE);
-    if (result == 0) {
-        print(buffer);
-        print("\n");
-    } else {
-        print("Error: Could not read file.\n");
-    }
+    print("Error: Could not read file.\n");
 }
 
-void shell_delete_file(const char *name) {
-    int file_index = -1;
-    for (int i = 0; i < MAX_FILES; i++) {
-        if (my_strcmp(fs.files[i].name, name) == 0) {
-            file_index = i;
-            break;
+void shell_delete(const char *name) {
+    int file_index = find_file_index(name);
+    if (file_index != -1) {
+        int result = delete_file(file_index);
+        if (result == 0) {
+            print("File deleted successfully.\n");
+            return;
         }
     }
-
-    if (file_index == -1) {
-        print("Error: File not found.\n");
-        return;
-    }
-
-    int result = delete_file(file_index);
-    if (result == 0) {
-        print("File deleted successfully.\n");
-    } else {
-        print("Error: Could not delete file.\n");
-    }
+    print("Error: Could not delete file.\n");
 }
 
 void shell_execute_command(const char *command) {
@@ -121,47 +94,52 @@ void shell_execute_command(const char *command) {
         return;
     }
 
-    if (my_strcmp(token, "help") == 0) {
-        shell_help();
-    } else if (my_strcmp(token, "echo") == 0) {
-        token = my_strtok(NULL, "");
-        if (token != NULL) {
-            shell_echo(token);
-        } else {
-            print("echo: missing argument\n");
-        }
-    } else if (my_strcmp(token, "clear") == 0) {
-        shell_clear();
-    } else if (my_strcmp(token, "create") == 0) {
-        token = my_strtok(NULL, "");
-        if (token != NULL) {
-            shell_create_file(token);
-        } else {
-            print("create: missing filename\n");
-        }
-    } else if (my_strcmp(token, "write") == 0) {
-        char *filename = my_strtok(NULL, " ");
-        char *data = my_strtok(NULL, "");
-        if (filename != NULL && data != NULL) {
-            shell_write_file(filename, data);
-        } else {
-            print("write: missing filename or data\n");
-        }
-    } else if (my_strcmp(token, "read") == 0) {
-        token = my_strtok(NULL, "");
-        if (token != NULL) {
-            shell_read_file(token);
-        } else {
-            print("read: missing filename\n");
-        }
-    } else if (my_strcmp(token, "delete") == 0) {
-        token = my_strtok(NULL, "");
-        if (token != NULL) {
-            shell_delete_file(token);
-        } else {
-            print("delete: missing filename\n");
-        }
-    } else {
-        print("Command not found. Type 'help' for a list of commands.\n");
+    switch (token[0]) {
+        case 'h':
+            shell_help();
+            break;
+        case 'e':
+            token = my_strtok(NULL, "");
+            if (token != NULL) {
+                shell_echo(token);
+            } else {
+                print("echo: missing argument\n");
+            }
+            break;
+        case 'c':
+            token = my_strtok(NULL, "");
+            if (token != NULL) {
+                shell_create(token);
+            } else {
+                print("create: missing filename\n");
+            }
+            break;
+        case 'w':
+            token = my_strtok(NULL, "");
+            if (token != NULL) {
+                shell_write(token);
+            } else {
+                print("write: missing filename or data\n");
+            }
+            break;
+        case 'r':
+            token = my_strtok(NULL, "");
+            if (token != NULL) {
+                shell_read(token);
+            } else {
+                print("read: missing filename\n");
+            }
+            break;
+        case 'd':
+            token = my_strtok(NULL, "");
+            if (token != NULL) {
+                shell_delete(token);
+            } else {
+                print("delete: missing filename\n");
+            }
+            break;
+        default:
+            print("Command not found. Type 'h' for a list of commands.\n");
+            break;
     }
 }
