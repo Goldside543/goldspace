bc2399ccc7f1c4dde0e1378abc5f6ef573926659
Goldside543 <goldside000@outlook.com>
2024-07-16 00:13:02 -0400

    Update string.h

--- kernel/string.h
+++ kernel/string.h
@@ -2,12 +2,12 @@
 #define STRING_H
 
 short my_strlen(const char *__s);
-int my_strcmp(const char* s1, const char* s2);
+int my_strcmp(const char *s1, const char *s2);
 char *my_strcat(char *dest, const char *src);
-char *my_strchr(const char *s, int c);
+char *my_strchr(const char *str, int c);
 char *my_strcpy(char *dest, const char* src);
 short my_strspn(const char *s1, const char *s2);
-char* my_strtok(char* str, const char* delim);
+char *my_strtok(char *str, const char *delim);
 short my_strcspn(const char *s1, const char *s2);
 char *my_strpbrk(const char *str1, const char *str2);
 
