3126075dc6a1071024360f454c01e6a5d1415500
Goldside543 <goldside000@outlook.com>
2024-07-16 13:25:13 -0400

    Update string.c

--- kernel/string.c
+++ kernel/string.c
@@ -111,3 +111,18 @@ short my_strcspn(const char *s1, const char *s2)
             s1++,ret++;
     return ret;
 }
+
+char* my_strncpy(char* dest, const char* src, size_t n) {
+    size_t i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+
+    // If src has less than n characters, append null bytes to the end of dest
+    for (; i < n; i++) {
+        dest[i] = '\0';
+    }
+
+    return dest;
+}
