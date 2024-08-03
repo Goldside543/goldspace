cfcf06adb00bf4174b6aa506a644312a9f67e45c
Goldside543 <goldside000@outlook.com>
2024-08-03 13:27:24 -0400

    Removed keyboard_init

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,12 +1,6 @@
 #include "usb.h"
 #include "keyboard.h"
 
-// Initialize keyboard
-void keyboard_init(void) {
-    usb_init(); // Initialize USB subsystem
-    // Additional keyboard-specific initialization can go here
-}
-
 // Read a character from the keyboard
 char read_keyboard(void) {
     volatile unsigned int *keyboard_status = (volatile unsigned int *)USB_KEYBOARD_STATUS;
