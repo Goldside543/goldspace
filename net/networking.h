71e84e228d8eda07bf9c4f1ac9f2561ec12f83d7
Goldside543 <goldside000@outlook.com>
2024-08-03 00:30:56 -0400

    Create networking.h

---
+++ net/networking.h
@@ -0,0 +1,28 @@
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
+// Define a structure for a network packet
+typedef struct {
+    unsigned char *data;
+    size_t length;
+} net_packet_t;
+
+// Define a structure for a network interface
+typedef struct {
+    char name[32];
+    unsigned char mac_address[6];
+    unsigned int ip_address;
+    unsigned int netmask;
+} net_interface_t;
+
+// Define a structure for a socket
+typedef struct {
+    int socket_fd;
+    unsigned int local_ip;
+    unsigned int remote_ip;
+    unsigned short local_port;
+    unsigned short remote_port;
+    int state;
+} net_socket_t;
+
+#endif // NETWORKING_H
