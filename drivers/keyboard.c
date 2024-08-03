9ef82a1d635ab6e3760d5b0f9f64f834ede62246
Goldside543 <goldside000@outlook.com>
2024-08-03 15:58:10 -0400

    Update keyboard.c

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -41,3 +41,16 @@ void init_keyboard(void) {
 
     print("Keyboard initialization complete.\n");
 }
+
+char read_keyboard(void) {
+    volatile unsigned int *keyboard_status = (volatile unsigned int *)USB_KEYBOARD_STATUS;
+    volatile unsigned int *keyboard_data = (volatile unsigned int *)USB_KEYBOARD_DATA;
+
+    // Wait for data to be ready
+    while (!(*keyboard_status & USB_KEYBOARD_STATUS_READY)) {
+        // Wait until data is ready
+    }
+
+    // Read the keyboard data
+    return (char)(*keyboard_data);
+}
