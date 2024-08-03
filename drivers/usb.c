d1803c57c6ff2e33855cfdf7bcbba39becd37ff3
Goldside543 <goldside000@outlook.com>
2024-08-03 13:43:11 -0400

    Adding dynamic drivers

--- drivers/usb.c
+++ drivers/usb.c
@@ -1,4 +1,8 @@
 #include "usb.h"
+#include "keyboard.h"  // Ensure this header file contains the declaration for keyboard_read
+
+// Define this variable to control which method to use
+static bool use_keyboard_driver = false;
 
 void usb_init(void) {
     volatile unsigned int *usb_ctrl = (volatile unsigned int *)USB_CTRL_REG;
@@ -11,7 +15,10 @@ void usb_init(void) {
     unsigned int timeout = 1000000;
     while (!(*usb_status & USB_STATUS_READY)) {
         if (--timeout == 0) {
+            use_keyboard_driver = false;  // Timeout occurred, set to use direct I/O
             return;
         }
     }
+
+    use_keyboard_driver = true;  // USB controller found, use keyboard driver
 }
