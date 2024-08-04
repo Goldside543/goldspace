21b8e97c0212f80930c2e2e00a6dcef6eef73499
Goldside543 <goldside000@outlook.com>
2024-08-03 23:42:22 -0400

    Update graphics.h

--- drivers/graphics.h
+++ drivers/graphics.h
@@ -1,7 +1,14 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <stdint.h>
+
+// Function prototypes
 void init_graphics(void);
-void draw_pixel(int x, int y, uint8_t color);
+void draw_rectangle(int x, int y, int width, int height, uint16_t color);
+void draw_line(int x1, int y1, int x2, int y2, uint16_t color);
+
+// Helper function for setting a pixel, used internally
+static void set_pixel(int x, int y, uint16_t color);
 
 #endif // GRAPHICS_H
