3b6c4f0906200f911881fa0e4dfc749d22908ac7
Goldside543 <goldside000@outlook.com>
2024-08-04 17:13:55 -0400

    Create print64.h

---
+++ kernel/print64.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+// Function to print a string to the VGA text mode display
+void print(const char *str);
+
+#endif // PRINT_H
