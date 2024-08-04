095d51978b29f8d402de64f4b7004126848c28f3
Goldside543 <goldside000@outlook.com>
2024-08-03 23:24:19 -0400

    Update graphics.c

--- drivers/graphics.c
+++ drivers/graphics.c
@@ -1,14 +1,17 @@
 #include <stdint.h>
+#include "../kernel/print.h"
 
 #define FRAMEBUFFER_ADDR 0xB8000
 #define SCREEN_WIDTH  80
 #define SCREEN_HEIGHT 25
 
 void init_graphics() {
+    print("Loading graphics driver...")
     uint16_t *framebuffer = (uint16_t *)FRAMEBUFFER_ADDR;
     for (int y = 0; y < SCREEN_HEIGHT; ++y) {
         for (int x = 0; x < SCREEN_WIDTH; ++x) {
             framebuffer[y * SCREEN_WIDTH + x] = (0x0F << 8) | ' ';
+            print("Graphics driver loaded.")
         }
     }
 }
