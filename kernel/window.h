baf393a781edc278cbc18e724803850880f9f5bc
Goldside543 <goldside000@outlook.com>
2024-08-03 23:47:35 -0400

    Create window.h

---
+++ kernel/window.h
@@ -0,0 +1,16 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "../drivers/graphics.h"
+
+// Define a simple window structure
+typedef struct {
+    int x, y, width, height;
+    uint16_t border_color;
+    uint16_t fill_color;
+} Window;
+
+// Function prototype for drawing a window
+void draw_window(Window *win);
+
+#endif // WINDOW_H
