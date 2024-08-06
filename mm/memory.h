89a16e1b1eca3d993de87ecc623b3eb4317a2aec
Goldside543 <goldside000@outlook.com>
2024-08-05 22:36:11 -0400

    Update memory.h

--- mm/memory.h
+++ mm/memory.h
@@ -9,4 +9,10 @@ void* kmalloc(size_t size);
 // Free previously allocated memory
 void kfree(void* ptr);
 
+// Set a block of memory to a specific value
+void* memset(void* ptr, int value, size_t num);
+
+// Copy a block of memory from source to destination
+void* memcpy(void* dest, const void* src, size_t num);
+
 #endif // MEMORY_H
