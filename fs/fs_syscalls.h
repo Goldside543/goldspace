c6d43c30870e262d8f33d1d5c3ecdea69c119320
Goldside543 <goldside000@outlook.com>
2024-08-06 12:33:16 -0400

    Update fs_syscalls.h

--- fs/fs_syscalls.h
+++ fs/fs_syscalls.h
@@ -3,12 +3,6 @@
 
 #include <stddef.h>
 
-// Define syscall numbers
-#define SYSCALL_CREATE_FILE  0
-#define SYSCALL_WRITE_FILE   1
-#define SYSCALL_READ_FILE    2
-#define SYSCALL_DELETE_FILE  3
-
 int sys_create_file(const char* name);
 int sys_write_file(int file_index, const char* data, int size);
 int sys_read_file(int file_index, char* buffer, int size);
