da76cf277a917d0bb7e1f8ec055e7a78a4f7b457
Goldside543 <goldside000@outlook.com>
2024-08-05 22:41:14 -0400

    Update memory.c

--- mm/memory.c
+++ mm/memory.c
@@ -49,3 +49,14 @@ void* kmemcpy(void* dest, const void* src, size_t num) {
     }
     return dest;
 }
+
+int kmemcmp(const void* ptr1, const void* ptr2, size_t num) {
+    const unsigned char* p1 = (const unsigned char*)ptr1;
+    const unsigned char* p2 = (const unsigned char*)ptr2;
+    for (size_t i = 0; i < num; ++i) {
+        if (p1[i] != p2[i]) {
+            return p1[i] - p2[i];
+        }
+    }
+    return 0;
+}
