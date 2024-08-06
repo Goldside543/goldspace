8e2c459d3a668ef42a0baed3b66f3ccd9ed1222d
Goldside543 <goldside000@outlook.com>
2024-08-06 12:50:15 -0400

    Update syscall_dispatcher.h

--- kernel/syscall_dispatcher.h
+++ kernel/syscall_dispatcher.h
@@ -1,9 +1,10 @@
 #ifndef SYSCALL_DISPATCHER_H
 #define SYSCALL_DISPATCHER_H
 
+#include "syscall_numbers.h"
 #include <stdint.h>
 
-// Function to handle system calls
-int syscall_dispatcher(uint32_t syscall_number, void* args);
+// Function prototype for the syscall handler
+int syscall_handler(int syscall_number, void* arg1, void* arg2, void* arg3, void* arg4);
 
 #endif // SYSCALL_DISPATCHER_H
