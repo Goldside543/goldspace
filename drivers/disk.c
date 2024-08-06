3412c9980c747ed2874e7fa7c0588a622b70d2bd
Goldside543 <goldside000@outlook.com>
2024-08-05 22:17:44 -0400

    Update disk.c

--- drivers/disk.c
+++ drivers/disk.c
@@ -1,24 +1,6 @@
 #include "../kernel/io.h"
 #include "disk.h"
 
-#define ATA_REG_DATA    0x1F0
-#define ATA_REG_ERROR   0x1F1
-#define ATA_REG_FEATURE 0x1F1
-#define ATA_REG_SECTOR_COUNT 0x1F2
-#define ATA_REG_LBA_LOW 0x1F3
-#define ATA_REG_LBA_MID 0x1F4
-#define ATA_REG_LBA_HIGH 0x1F5
-#define ATA_REG_DRIVE_SELECT 0x1F6
-#define ATA_REG_COMMAND 0x1F7
-#define ATA_REG_STATUS 0x1F7
-
-#define ATA_COMMAND_READ_SECTORS 0x20
-#define ATA_COMMAND_WRITE_SECTORS 0x30
-#define ATA_STATUS_BUSY 0x80
-#define ATA_STATUS_DRQ 0x08
-#define ATA_STATUS_READY 0x40
-#define ATA_STATUS_ERROR 0x01
-
 void ata_pio_select_drive(uint8_t drive) {
     outb(ATA_REG_DRIVE_SELECT, 0xA0 | (drive << 4));
 }
