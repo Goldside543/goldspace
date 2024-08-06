68f270bfeb41d7ada380e35f00e895967d98cfa5
Goldside543 <goldside000@outlook.com>
2024-08-05 22:35:03 -0400

    Upgrade memory management

--- mm/memory.c
+++ mm/memory.c
@@ -1,9 +1,12 @@
 #include <stddef.h>
+#include <stdint.h>
+
 #define MEMORY_POOL_SIZE 1024
 
 static unsigned char memory_pool[MEMORY_POOL_SIZE];
 static unsigned int memory_index = 0;
 
+/* Allocate memory from the pool */
 void* kmalloc(size_t size) {
     if (memory_index + size > MEMORY_POOL_SIZE) {
         return NULL; // Out of memory
@@ -15,4 +18,21 @@ void* kmalloc(size_t size) {
 
 void kfree(void* ptr) {
     // Simple implementation: Do nothing, as we are not freeing individual blocks
+}
+
+void* memset(void* ptr, int value, size_t num) {
+    unsigned char* p = (unsigned char*)ptr;
+    while (num--) {
+        *p++ = (unsigned char)value;
+    }
+    return ptr;
+}
+
+void* memcpy(void* dest, const void* src, size_t num) {
+    unsigned char* d = (unsigned char*)dest;
+    const unsigned char* s = (const unsigned char*)src;
+    while (num--) {
+        *d++ = *s++;
+    }
+    return dest;
 }
