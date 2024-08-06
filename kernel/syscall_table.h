6589a908fddac2ee2c2ce17e8752c8c71f0b9307
Goldside543 <goldside000@outlook.com>
2024-08-06 12:37:59 -0400

    Create syscall_table.h

---
+++ kernel/syscall_table.h
@@ -0,0 +1,13 @@
+#ifndef SYSCALL_TABLE_H
+#define SYSCALL_TABLE_H
+
+#include "syscall_numbers.h"
+#include "syscall_handlers.h"
+
+// Define the syscall table
+extern int (*syscall_table[])(void);
+
+// Function to initialize the syscall table
+void init_syscall_table();
+
+#endif // SYSCALL_TABLE_H
