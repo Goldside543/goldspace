959af087cc5611dc9aec9722c6c1439265a0364a
Goldside543 <goldside000@outlook.com>
2024-08-06 12:53:52 -0400

    Update syscall_table.h

--- kernel/syscall_table.h
+++ kernel/syscall_table.h
@@ -2,12 +2,15 @@
 #define SYSCALL_TABLE_H
 
 #include "syscall_numbers.h"
-#include "../fs/fs_syscalls.h"
 
-// Define the syscall table
-extern int (*syscall_table[])(void);
+int sys_create_file(void* name, void* unused1, void* unused2, void* unused3);
+int sys_write_file(void* file_index, void* data, void* size, void* unused);
+int sys_read_file(void* file_index, void* buffer, void* size, void* unused);
+int sys_delete_file(void* file_index, void* unused1, void* unused2, void* unused3);
 
-// Function to initialize the syscall table
+// Declare the syscall table
+extern int (*syscall_table[])(void*, void*, void*, void*);
+
 void init_syscall_table();
 
 #endif // SYSCALL_TABLE_H
