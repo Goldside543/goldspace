ea0511a929348f510301dc605381a166c66b9648
Goldside543 <goldside000@outlook.com>
2024-08-14 13:09:19 -0400

    Update rtc.c

--- drivers/rtc.c
+++ drivers/rtc.c
@@ -2,13 +2,13 @@
 #include "../kernel/io.h"
 
 // Read a register from the RTC
-static uint8_t rtc_read(uint8_t reg) {
+uint8_t rtc_read(uint8_t reg) {
     outb(RTC_INDEX_PORT, reg);
     return inb(RTC_DATA_PORT);
 }
 
 // Convert BCD to binary
-static uint8_t bcd_to_bin(uint8_t bcd) {
+uint8_t bcd_to_bin(uint8_t bcd) {
     return (bcd >> 4) * 10 + (bcd & 0x0F);
 }
 
