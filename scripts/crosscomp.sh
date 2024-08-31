ed55d746a99283bec3c652541637c299a637e836
Goldside543 <goldside000@outlook.com>
2024-08-31 10:35:34 -0400

    Update crosscomp.sh

--- scripts/crosscomp.sh
+++ scripts/crosscomp.sh
@@ -13,3 +13,12 @@ sed -i 's/\bgcc\b/i686-linux-gnu-gcc/g' Makefile
 sed -i 's/\bld\b/i686-linux-gnu-ld/g' Makefile
 
 echo "Replaced 'gcc' with 'i686-linux-gnu-gcc' and 'ld' with 'i686-linux-gnu-ld' in the Makefile."
+
+# Check if the kernel/linker.ld file exists
+if [ -f kernel/linker.ld ]; then
+    # Rename the linker.ld file
+    mv kernel/linker.ld kernel/linker.i686-linux-gnu-ld
+    echo "Renamed 'kernel/linker.ld' to 'kernel/linker.i686-linux-gnu-ld'."
+else
+    echo "File 'kernel/linker.ld' not found, no renaming was done."
+fi
