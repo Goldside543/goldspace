3c8cd2b5cc9533c89f82a68a8c129e9f432043b3
Goldside543 <goldside000@outlook.com>
2024-08-31 10:29:06 -0400

    Update crosscomp.sh

--- scripts/crosscomp.sh
+++ scripts/crosscomp.sh
@@ -9,4 +9,7 @@ fi
 # Replace gcc with i686-linux-gnu-gcc
 sed -i 's/\bgcc\b/i686-linux-gnu-gcc/g' Makefile
 
-echo "Replaced 'gcc' with 'i686-linux-gnu-gcc' in the Makefile."
+# Replace ld with i686-linux-gnu-ld
+sed -i 's/\bld\b/i686-linux-gnu-ld/g' Makefile
+
+echo "Replaced 'gcc' with 'i686-linux-gnu-gcc' and 'ld' with 'i686-linux-gnu-ld' in the Makefile."
