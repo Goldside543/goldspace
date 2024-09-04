e471306d83844386d2907610a4504e06a915013d
Steven Davis <goldside000@outlook.com>
2024-09-04 13:29:08 -0400

    Expanded Rust library

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,9 @@
 use core::panic::PanicInfo;
 use core::arch::asm;
 
+static mut LOG_BUFFER: [u8; 1024] = [0; 1024];
+static mut LOG_INDEX: usize = 0;
+
 #[panic_handler]
 #[no_mangle]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,4 +16,19 @@ fn panic(_info: &PanicInfo) -> ! {
             asm!("hlt");
         }
     }
+}
+
+pub fn log_message(message: &str) {
+    unsafe {
+        for byte in message.as_bytes() {
+            if LOG_INDEX < LOG_BUFFER.len() {
+                LOG_BUFFER[LOG_INDEX] = *byte;
+                LOG_INDEX += 1;
+            }
+        }
+    }
+}
+
+pub fn get_log_buffer() -> &'static [u8] {
+    unsafe { &LOG_BUFFER[..LOG_INDEX] }
 }
