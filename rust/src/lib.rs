d9626b89252d12aed78f8eda5f589ac324bcf2a9
Goldside543 <goldside000@outlook.com>
2024-09-04 14:17:12 -0400

    Changed path on copyright header to full path

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*!
- * src/lib.rs
+ * rust/src/lib.rs
  *
  * Goldspace's Rust library, where Rust functions can
  * be used in the kernel. Seeing as Rust usage in the
