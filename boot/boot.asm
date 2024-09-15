6483190465610e502d61884bad0d669cd5e12490
Steven Davis <goldside000@outlook.com>
2024-09-15 17:54:42 -0400

    Added memory checking to boot

--- boot/boot.asm
+++ boot/boot.asm
@@ -16,7 +16,7 @@ start:
     mov es, ax
     mov ss, ax
     mov sp, 0x7c00
- 
+
     ; Load the kernel into memory
     mov bx, 0x1000     ; Address to load the kernel
     mov ah, 0x02       ; BIOS read sector function
@@ -25,6 +25,22 @@ start:
     mov cl, 2          ; Sector (starting from 2)
     mov dh, 0          ; Head
     int 0x13
+
+    ; Load segment register ES with value 0x1000
+    mov ax, 0x1000    ; Load the segment value into AX
+    mov es, ax        ; Transfer the value from AX to ES
+
+    ; Write a value to a specific memory location
+    mov di, 0x0000         ; Offset address within the segment
+    mov al, 0xAA           ; Value to write
+    mov [es:di], al        ; Write value to memory
+
+    ; Read the value back from the same memory location
+    mov al, [es:di]        ; Read value from memory
+
+    ; Compare the read value with the original value
+    cmp al, 0xAA           ; Compare with expected value
+    jne memory_error       ; Jump if not equal (error)
 
     ; Switch to protected mode
     cli                ; Disable interrupts
@@ -36,6 +52,25 @@ start:
     mov cr0, eax
 
     jmp CODE_SEG:init_protected_mode
+
+memory_error:
+    ; Set video mode to 320x200 256-color (Mode 13h) for error display
+    mov ax, 0x0013
+    int 0x10
+
+    ; Load segment register ES with value 0xA000 (Video memory segment)
+    mov ax, 0xA000    ; Load the segment value into AX
+    mov es, ax        ; Transfer the value from AX to ES
+
+    ; Fill the screen with light green color (color index 2)
+    mov cx, 0xFA00      ; Number of pixels (320x200)
+    mov al, 0x02        ; Light green color index
+    mov di, 0x0000      ; Video memory segment
+    rep stosb           ; Fill video memory with color
+
+    ; Halt the system
+    cli                 ; Clear interrupts
+    hlt                 ; Halt CPU
 
 gdt_start:
     ; GDT (Global Descriptor Table)
