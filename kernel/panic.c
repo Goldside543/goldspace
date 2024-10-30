9c9760e2275eeecd480f90d27f407d347604faeb
Goldside543 <goldside000@outlook.com>
2024-10-30 01:24:09 -0400

    Update panic.c

--- kernel/panic.c
+++ kernel/panic.c
@@ -8,7 +8,16 @@
  *
  */
 
+#include "print.h"
+
 void panic() {
+    // Print error message
+    print("\n");
+    print("ERROR!!! KERNEL HAS ENCOUNTERED SEVERE,\n");
+    print("IRRECOVERABLE ERROR!!! PLEASE REPORT THIS\n");
+    print("ERROR TO EITHER AN OS MAINTAINER OR KERNEL\n");
+    print("MAINTAINER, THE LATTER AT\n");
+    print("goldside000@outlook.com");
     // Halt the CPU with an infinite loop
     while (1) {
         __asm__ volatile ("nop");
