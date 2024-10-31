0f92d5232d40116aa9468c5ee69e2dc7f44b87d2
Goldside543 <goldside000@outlook.com>
2024-10-30 23:33:43 -0400

    Update version number

--- kernel/core.c
+++ kernel/core.c
@@ -236,7 +236,7 @@ void kernel_main() {
     print("Type 'help' for available commands.\n");
     print("\n");
 
-    print("Goldspace v2.9.7, Copyright (C) 2024 Goldside543\n");
+    print("Goldspace v2.9.8, Copyright (C) 2024 Goldside543\n");
     print("Goldspace comes with ABSOLUTELY NO WARRANTY.\n");
     print("This is free software, and you are welcome to redistribute it\n");
     print("under certain conditions. See the GPL-2.0 license for details.\n");
