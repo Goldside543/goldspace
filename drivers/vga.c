37638f8982fb9f208e31514cb194c06ab078bf92
Goldside543 <goldside000@outlook.com>
2024-11-02 20:56:03 -0400

    Update vga.c

--- drivers/vga.c
+++ drivers/vga.c
@@ -20,6 +20,7 @@
 #define VGA_GC_DATA    0x3CF
 #define VGA_AC_INDEX   0x3C0
 #define VGA_AC_DATA    0x3C1
+#define VGA_MISC_WRITE 0x3C2
 #define VGA_MEMORY     0xA000
 
 // Mode 13h constants
@@ -36,27 +37,39 @@ void clear_screen() {
 
 // Function to set VGA mode 13h
 void set_mode_13h() {
+    // Miscellaneous output register
+    outb(VGA_MISC_WRITE, 0x63);
+
     // Unlock CRTC registers
     outb(VGA_CRTC_INDEX, 0x03);  // Disable screen
     outb(VGA_CRTC_DATA, 0x80);   // Protect registers
 
-    // Set Mode 13h in the sequencer
-    outb(VGA_SEQ_INDEX, 0x00);
-    outb(VGA_SEQ_DATA, 0x03); // Unlock
+    // Set sequencer registers for mode 13h
+    outb(VGA_SEQ_INDEX, 0x00); // Reset sequencer
+    outb(VGA_SEQ_DATA, 0x03);  
     outb(VGA_SEQ_INDEX, 0x01);
-    outb(VGA_SEQ_DATA, 0x01); // Mode 13h initialization
+    outb(VGA_SEQ_DATA, 0x01);  // Clocking mode for 320x200
+    outb(VGA_SEQ_INDEX, 0x04); // Memory mode
+    outb(VGA_SEQ_DATA, 0x0E);  // Enable plane A
 
     // Set CRTC registers for 320x200 resolution
-    outb(VGA_CRTC_INDEX, 0x11);
-    outb(VGA_CRTC_DATA, 0xE3);
+    outb(VGA_CRTC_INDEX, 0x11);  // Vertical retrace end
+    outb(VGA_CRTC_DATA, 0x0C);   // Clear bit 7 for mode 13h
+    outb(VGA_CRTC_INDEX, 0x13);  // Underline location register
+    outb(VGA_CRTC_DATA, 0x20);   // Set to 0x20 for mode 13h
 
     // Graphics controller settings for mode 13h
-    outb(VGA_GC_INDEX, 0x06);
-    outb(VGA_GC_DATA, 0x05); // Select plane
+    outb(VGA_GC_INDEX, 0x05); // Graphics mode
+    outb(VGA_GC_DATA, 0x40);  // Shift 256-color mode
+    outb(VGA_GC_INDEX, 0x06); // Miscellaneous graphics register
+    outb(VGA_GC_DATA, 0x05);  // Graphics mode settings
 
     // Attribute controller settings
-    outb(VGA_AC_INDEX, 0x20); // Set mode
-    outb(VGA_AC_DATA, 0x00);
+    for (int i = 0; i < 16; i++) {
+        outb(VGA_AC_INDEX, i);
+        outb(VGA_AC_DATA, i);
+    }
+    outb(VGA_AC_INDEX, 0x20);  // End of attribute controller sequence
 
     clear_screen();
 }
