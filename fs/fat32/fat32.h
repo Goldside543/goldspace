6f272fbfe0e2ba98260831ac1ef90c27d2825383
Goldside543 <goldside000@outlook.com>
2024-11-04 01:50:50 -0500

    Create fat32.h

---
+++ fs/fat32/fat32.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef FAT32_H
+#define FAT32_H
+
+bool fat32_mount();
+bool fat32_read_file(const char *filename, void *buffer, size_t size);
+bool fat32_write_file(const char *filename, const void *buffer, size_t size);
+static uint32_t get_cluster_size();
+static uint32_t get_fat_entry(uint32_t cluster);
+static void set_fat_entry(uint32_t cluster, uint32_t value);
+static uint32_t find_file(const char *filename);
+static void read_clusters(uint32_t start_cluster, void *buffer, size_t size);
+static uint32_t allocate_clusters(size_t size);
+static void write_clusters(uint32_t start_cluster, const void *buffer, size_t size);
+static void update_directory_entry(const char *filename, uint32_t start_cluster, size_t size);
+
+#endif FAT32_H
