fca1436272724ccb9e17bdad48dc4023198d2b92
Goldside543 <goldside000@outlook.com>
2024-11-04 01:51:34 -0500

    Update fat32.c

--- fs/fat32/fat32.c
+++ fs/fat32/fat32.c
@@ -8,11 +8,10 @@
  *
  */
 
-#include <stdint.h>
+#include "fat32.h"
 #include "../../kernel/string.h"
 #include "../../drivers/disk.h"
 #include "../../mm/memory.h"
-#include <stdbool.h>
 
 #define SECTOR_SIZE 512
 #define FAT32_MAX_FILES 128
@@ -64,19 +63,6 @@ typedef struct {
 static FAT32_BootSector boot_sector;
 static uint32_t fat_start;
 static uint32_t data_start;
-
-// Function prototypes
-bool fat32_mount();
-bool fat32_read_file(const char *filename, void *buffer, size_t size);
-bool fat32_write_file(const char *filename, const void *buffer, size_t size);
-static uint32_t get_cluster_size();
-static uint32_t get_fat_entry(uint32_t cluster);
-static void set_fat_entry(uint32_t cluster, uint32_t value);
-static uint32_t find_file(const char *filename);
-static void read_clusters(uint32_t start_cluster, void *buffer, size_t size);
-static uint32_t allocate_clusters(size_t size);
-static void write_clusters(uint32_t start_cluster, const void *buffer, size_t size);
-static void update_directory_entry(const char *filename, uint32_t start_cluster, size_t size);
 
 // Mount the FAT32 file system
 bool fat32_mount() {
