7c27e5d47438d0b1175a56b132694905bfadd360
Goldside543 <goldside000@outlook.com>
2024-12-28 16:31:47 -0500

    Update fs_syscalls.c

--- fs/fs_syscalls.c
+++ fs/fs_syscalls.c
@@ -10,11 +10,11 @@
 
 #include "../kernel/syscall_numbers.h"
 #include "../kernel/print.h"
-#include "bffs/bffs.h" 
+#include "vfs/vfs.h" 
 
 // System call handler for creating a file
 int sys_create_file(const char* name) {
-    int result = create_file(name);
+    int result = vfs_create_file(name);
     if(result<0){
         print("No space left in the file table.\n");
     }
@@ -22,15 +22,15 @@ int sys_create_file(const char* name) {
 
 // System call handler for writing to a file
 int sys_write_file(int file_index, const char* data, int size) {
-    int result = write_file(file_index, data, size);
+    int result = vfs_write_file(file_index, data, size);
     if(result<0){
-        print("No free blocks available, data size is too large or file doesn't exist\n"); // Error codes are planned in the future.
+        print("No free blocks available, data size is too large or file doesn't exist\n");
     }
 }
 
 // System call handler for reading from a file
 int sys_read_file(int file_index, char* buffer, int size) {
-    int result = read_file(file_index, buffer, size);
+    int result = vfs_read_file(file_index, buffer, size);
     if(result<0){
         print("File doesn't exist or no data written.\n");
     }
@@ -38,7 +38,7 @@ int sys_read_file(int file_index, char* buffer, int size) {
 
 // System call handler for deleting a file
 int sys_delete_file(int file_index) {
-    int result = delete_file(file_index);
+    int result = vfs_delete_file(file_index);
     if(result<0){
         print("File doesn't exist.\n");
     }
