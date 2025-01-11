e0dc905a155abf315f61160d154a3c25c78f303c
Goldside543 <goldside000@outlook.com>
2025-01-11 14:20:15 -0500

    Update core.c

--- kernel/core.c
+++ kernel/core.c
@@ -178,7 +178,6 @@ void keyboard_isr() {
 
     // Send an End of Interrupt (EOI) to the PIC
     outb(0x20, 0x20);  // EOI for Master PIC (IRQ1)
-    asm volatile("popa");
     asm volatile("iret");
 }
 
