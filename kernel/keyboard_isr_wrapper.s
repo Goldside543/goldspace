d8645cfd6fe4314d2fe9e3dcbabf1ecbf7a219b3
Goldside543 <goldside000@outlook.com>
2025-01-22 22:03:25 -0500

    Update keyboard_isr_wrapper.s

--- kernel/keyboard_isr_wrapper.s
+++ kernel/keyboard_isr_wrapper.s
@@ -4,5 +4,7 @@ keyboard_isr_wrapper:
     pushal
     cld              # C code following the sysV ABI requires DF to be clear on function entry
     call keyboard_isr
+    movb $0x20, %al
+    outb %al, $0x20
     popal
     iret
