07f82974e3183e1494a51139183e4ee34a0af71f
Goldside543 <goldside000@outlook.com>
2025-02-03 01:18:08 -0500

    Update privileges.s

--- kernel/privileges.s
+++ kernel/privileges.s
@@ -1,14 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 .global protect_tsc
-.global protect_iopl
 
 protect_tsc:
 	movl %cr4, %eax
 	orl $0x4, %eax
 	movl %eax, %cr4
-
-protect_iopl:
-	movl %eflags, %eax
-	andl $0xFFFFFFFC, %eax
-	movl %eax, %eflags
