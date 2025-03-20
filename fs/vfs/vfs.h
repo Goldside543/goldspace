3320b4f2b263775858d28218420b2ba4d3bcf723
Goldside543 <goldside000@outlook.com>
2025-03-19 21:48:43 -0400

    Update vfs.h

--- fs/vfs/vfs.h
+++ fs/vfs/vfs.h
@@ -9,12 +9,12 @@ void register_fs(FileSystem *fs);
 
 int vfs_mount(const char *fs_name, const char *device);
 
-int vfs_open(const char *path, int flags);
+int vfs_open(const char *path, int flags, void* unused1, void* unused2);
 
-ssize_t vfs_read(int fd, void *buf, size_t size);
+ssize_t vfs_read(int fd, void *buf, size_t size, void* unused1);
 
-ssize_t vfs_write(int fd, const void *buf, size_t size);
+ssize_t vfs_write(int fd, const void *buf, size_t size, void* unused1);
 
-int vfs_close(int fd);
+int vfs_close(int fd, void* unused1, void* unused2, void* unused3);
 
 #endif // VFS_H
