898b2d0838356705f83e416f3cbd6f562bab4d48
Goldside543 <goldside000@outlook.com>
2025-04-02 02:25:56 -0400

    Create serial.h

---
+++ drivers/serial.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SERIAL_H
+#define SERIAL_H
+
+static int init_serial();
+char read_serial();
+void write_serial(char a);
+
+#endif // SERIAL_H
