78698b1d6a8639f214b451d89441f06ec26d41f9
Goldside543 <goldside000@outlook.com>
2025-04-09 01:12:33 -0400

    Update gpf_isr_wrapper.s

--- kernel/gpf_isr_wrapper.s
+++ kernel/gpf_isr_wrapper.s
@@ -11,11 +11,9 @@ gpf_isr_wrapper:
   popl %eax
   popl %eax
   movl %eax, saved_cs
-  and $0x3, saved_cs
+  andl $0x3, saved_cs
   pushal
   cld              # C code following the sysV ABI requires DF to be clear on function entry
   call gpf_handler
   popal
   iret
-
-.section .note.GNU-stack,"",@progbits
