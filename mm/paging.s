7c3f083d9fb2a82d92c7f15b8125a1c4752e13b3
Goldside543 <goldside000@outlook.com>
2025-05-08 00:54:19 -0400

    Update paging.s

--- mm/paging.s
+++ mm/paging.s
@@ -12,47 +12,62 @@ page_directory:
     .fill 1024, 4, 0              # Page Directory: 1024 entries (4KB)
 
 page_tables:
-    .fill 1024 * 5, 4, 0          # Enough for CODE, DATA, RO_DATA, VGA, stack, and IDT
+    .fill 1024 * 7, 4, 0          # Enough for CODE, DATA, RO_DATA, VGA, stack, IDT, and higher half
 
 .section .text
 
 # -------------------------------
 # init_paging:
-# - Sets up paging for kernel sections, VGA, stack, and IDT
+# - Sets up paging for kernel sections, VGA, stack, IDT, and higher half
 # -------------------------------
 init_paging:
     lea page_directory, %ebx           # %ebx = page_directory
     lea page_tables, %edi              # %edi = start of all page tables
 
-    # Map CODE section
+    # Map CODE section (identity map)
     movl $0x00100000, %eax             # Start of CODE section
     movl $0x0, %ecx                    # Page Directory Index (CODE)
     call map_section
 
-    # Map DATA section
+    # Map DATA section (identity map)
     movl $0x06000000, %eax             # Start of DATA section
     movl $1, %ecx                      # Page Directory Index (DATA)
     call map_section
 
-    # Map RO_DATA section
+    # Map RO_DATA section (identity map)
     movl $0x0C000000, %eax             # Start of RO_DATA section
     movl $2, %ecx                      # Page Directory Index (RO_DATA)
     call map_section
 
-    # Map VGA framebuffer (0xB80000)
+    # Map VGA framebuffer (identity map @ 0xB80000)
     movl $0xB80000, %eax               # Start of VGA framebuffer
     movl $3, %ecx                      # Page Directory Index (VGA)
     call map_single_page
 
-    # Map stack (from stack_addr)
+    # Map stack (identity map @ stack_addr)
     movl stack_addr, %eax              # Start of stack
     movl $4, %ecx                      # Page Directory Index (stack)
     call map_single_page
 
-    # Map IDT (from idt_addr)
+    # Map IDT (identity map @ idt_addr)
     movl idt_addr, %eax                # Start of IDT
     movl $5, %ecx                      # Page Directory Index (IDT)
     call map_single_page
+
+    # Map CODE section (higher half @ 0xC0000000)
+    movl $0x00100000, %eax             # Physical address of CODE
+    movl $768, %ecx                    # Page Directory Index (0xC0000000 / 4MB = 768)
+    call map_section
+
+    # Map DATA section (higher half @ 0xC4000000)
+    movl $0x06000000, %eax             # Physical address of DATA
+    movl $769, %ecx                    # Page Directory Index (0xC4000000 / 4MB = 769)
+    call map_section
+
+    # Map RO_DATA section (higher half @ 0xC8000000)
+    movl $0x0C000000, %eax             # Physical address of RO_DATA
+    movl $770, %ecx                    # Page Directory Index (0xC8000000 / 4MB = 770)
+    call map_section
 
     ret
 
