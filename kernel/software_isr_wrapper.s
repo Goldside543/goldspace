de1ed2ef63229a4df143853d0364fcc046782c5c
Goldside543 <goldside000@outlook.com>
2025-05-13 00:05:51 -0400

    Update software_isr_wrapper.s

--- kernel/software_isr_wrapper.s
+++ kernel/software_isr_wrapper.s
@@ -3,10 +3,6 @@
 .global software_isr_wrapper
 
 software_isr_wrapper:
-    pushal
     cld              # C code following the sysV ABI requires DF to be clear on function entry
     call software_interrupt_handler
-    movb $0x20, %al
-    outb %al, $0x20
-    popal
     iret
