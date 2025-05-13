0475348564f467955f7d297dbb4243e0d48c175e
Goldside543 <goldside000@outlook.com>
2025-05-12 23:58:39 -0400

    Update enter_user_mode.c

--- kernel/enter_user_mode.c
+++ kernel/enter_user_mode.c
@@ -9,8 +9,22 @@
  *
  */
 
+void puts(const char *str) {
+    asm volatile (
+        "mov $8, %%eax;"          // Load syscall number (8) into eax
+        "mov %%ebx, 4(%%ebp);"    // Load the address of the string into 4(%%ebp)
+        "mov %[str], %%ebx;"      // Move the string pointer to ebx (1st argument)
+        "int $0x80;"              // Make the syscall
+        :
+        : [str] "r"(str)          // Input: 'str' (the string argument)
+        : "%eax", "%ebx", "%ebp" // Clobbered registers
+    );
+}
+
 int run_user_space() {
-   while(1) {
-      asm volatile("nop"); // do nothing
+   puts("Hello, ring 3 and system calls!");
+
+   for (;;) {
+      asm volatile("nop");
    }
 }
