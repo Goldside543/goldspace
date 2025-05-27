b0d86431c46b85963f5af3a3576246f00add5d42
Goldside543 <goldside000@outlook.com>
2025-05-26 23:45:46 -0400

    Update paging.s

--- mm/paging.s
+++ mm/paging.s
@@ -4,123 +4,120 @@
 .global enable_paging
 
 .section .data
-    .align 0x1000
+.align 0x1000
 
 page_directory:
     .fill 1024, 4, 0
 
 page_tables:
-    .fill 1024 * 8, 4, 0        # Let's allocate 8 page tables for our mapped regions (adjust as needed)
+    .fill 1024 * 8, 4, 0        # 8 page tables (8 * 4KB * 1024 = 32MB)
 
 .section .text
 
-# Helper macros for mapping
+# Helper macro
 .macro MAP_REGION vaddr, paddr, size
-    # vaddr = virtual start, paddr = physical start, size = bytes
-    # Will map [vaddr, vaddr+size) to [paddr, paddr+size)
-    # Assumes %ebx = page_directory base, %edi = page_tables base
-
     pushl %eax
+    pushl %ebx
     pushl %ecx
     pushl %edx
+    pushl %esi
+    pushl %edi
 
-    movl $\vaddr, %eax      # Current virtual address
-    movl $\paddr, %edx      # Current physical address
-    movl $\size, %ecx       # Bytes left to map
+    movl $\vaddr, %esi      # virtual
+    movl $\paddr, %edi      # physical
+    movl $\size, %ecx       # total size to map
 
-.map_page_loop\@:
-    movl %eax, %esi
-    shrl $22, %esi                  # page directory index
-    movl %edi, %ebp
-    addl $(4096 * 1024), %ebp       # skip over identity-mapped tables
-    movl %ebp, %ebp                 # for clarity
-    movl %ebx, %ebp                 # directory base
-
-    # Set up page directory entry for this vaddr
-    movl %ebx, %ebp
-    movl %esi, %ebp
-    shll $2, %ebp
-    addl %ebx, %ebp
-    movl %ebp, %ebp                 # %ebp = &page_directory[pd_index]
-
-    # Set page directory entry if not present yet
-    movl (%ebp), %ebx
-    testl $1, %ebx
-    jnz .pd_entry_set\@
-    # Set page table pointer
-    movl %edi, (%ebp)
-    orl $0x3, (%ebp)
-.pd_entry_set\@:
-
-    # Now set page table entry
+.map_loop\@:
+    movl %esi, %eax
+    shrl $22, %eax                   # page directory index
     movl %eax, %ebx
-    shrl $12, %ebx                  # page table index
-    andl $0x3FF, %ebx
-    movl (%ebp), %ebp               # get page table address
-    andl $0xFFFFF000, %ebp
-    addl %ebx, %ebx
-    shll $2, %ebx
-    addl %ebx, %ebp
-    movl %edx, (%ebp)
-    orl $0x3, (%ebp)
+    shll $2, %ebx                    # * 4 for byte offset
+    lea page_directory, %edx
+    addl %ebx, %edx                  # &page_directory[index]
 
-    # Advance
-    addl $0x1000, %eax
-    addl $0x1000, %edx
+    # If page directory entry not set, assign page table
+    movl (%edx), %eax
+    testl $1, %eax
+    jnz .skip_pd\@
+
+    # Find next free page table slot
+    movl %eax, %eax                  # zero %eax
+    movl %esi, %ebx
+    shrl $22, %ebx
+    shll $12, %ebx                   # * 0x1000 (size of table)
+    lea page_tables, %eax
+    addl %ebx, %eax
+    orl $0x3, %eax                   # present + RW
+    movl %eax, (%edx)
+
+.skip_pd\@:
+    # Set page table entry
+    movl %esi, %eax
+    shrl $12, %eax                   # page table index
+    andl $0x3FF, %eax
+    shll $2, %eax                    # offset into table
+
+    movl (%edx), %ebx                # get page table addr
+    andl $0xFFFFF000, %ebx
+    addl %eax, %ebx
+
+    movl %edi, %eax
+    orl $0x3, %eax                   # present + RW
+    movl %eax, (%ebx)
+
+    addl $0x1000, %esi
+    addl $0x1000, %edi
     subl $0x1000, %ecx
-    jg .map_page_loop\@
+    jg .map_loop\@
 
+    popl %edi
+    popl %esi
     popl %edx
     popl %ecx
+    popl %ebx
     popl %eax
 .endm
 
 # -------------------------------
-# init_paging:
-# - Sets up identity & higher-half kernel paging
+# init_paging
 # -------------------------------
 init_paging:
-    lea page_directory, %ebx
+    # Clear page directory and page tables
+    lea page_directory, %edi
+    movl $0, %eax
+    movl $1024, %ecx
+.clear_pd:
+    stosl
+    loop .clear_pd
+
     lea page_tables, %edi
-
-    # Clear page directory and tables
     movl $0, %eax
-    movl $1024*4, %ecx
-    rep stosl
-    movl $0, %eax
-    movl $1024*8*4, %ecx     # 8 tables * 1024 entries * 4 bytes
-    rep stosl
+    movl $1024 * 8, %ecx
+.clear_pts:
+    stosl
+    loop .clear_pts
 
-    # Identity map Multiboot (0x00000000 - 0x00000FFF)
+    # Map identity regions
     MAP_REGION 0x00000000, 0x00000000, 0x1000
-
-    # Identity map Realmode (0x00080000 - 0x00087FFF)
     MAP_REGION 0x00080000, 0x00080000, 0x8000
-
-    # Identity map Kernel code (.text, .rodata): 0x00100000 - 0x008FFFFF (8MB)
     MAP_REGION 0x00100000, 0x00100000, 0x800000
-
-    # Identity map Kernel data (.data, .bss): 0x06000000 - 0x067FFFFF (8MB)
     MAP_REGION 0x06000000, 0x06000000, 0x800000
-
-    # Identity map Kernel rodata: 0x0C000000 - 0x0C7FFFFF (8MB)
     MAP_REGION 0x0C000000, 0x0C000000, 0x800000
 
-    # Map kernel to higher-half (virtual 0xC0000000 - 0xC07FFFFF -> physical 0x00100000 - 0x008FFFFF)
+    # Higher-half kernel mapping (0xC0000000 -> 0x00100000)
     MAP_REGION 0xC0000000, 0x00100000, 0x800000
 
     ret
 
 # -------------------------------
-# enable_paging:
-# - Loads CR3, sets PG bit in CR0
+# enable_paging
 # -------------------------------
 enable_paging:
     lea page_directory, %eax
-    movl %eax, %cr3                   # load page directory into CR3
+    movl %eax, %cr3         # load page directory
 
     movl %cr0, %eax
-    orl $0x80000000, %eax             # enable PG (bit 31)
+    orl $0x80000000, %eax   # set PG bit
     movl %eax, %cr0
 
     ret
