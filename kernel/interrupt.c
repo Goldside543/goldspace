e4bafc5a7e9727effe7ea64de03b56cd422a714e
Goldside543 <goldside000@outlook.com>
2025-06-04 21:44:08 -0400

    Update interrupt.c

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -12,28 +12,7 @@
 #include "syscall_dispatcher.h"
 
 // Interrupt handler for the software interrupt
-void software_interrupt_handler() {
-
-    // Get the system call number from EAX
-    int syscall_number;
-    asm volatile ("movl %%eax, %0" : "=r"(syscall_number));
-
-    // Retrieve arguments from the stack
-    void* arg1;
-    void* arg2;
-    void* arg3;
-    void* arg4;
-
-    asm volatile (
-        "movl %%ebx, %0\n"   // Get the first argument (arg1)
-        "movl %%ecx, %1\n"   // Get the second argument (arg2)
-        "movl %%edx, %2\n"  // Get the third argument (arg3)
-        "movl %%esi, %3"    // Get the fourth argument (arg4)
-        : "=r"(arg1), "=r"(arg2), "=r"(arg3), "=r"(arg4)
-        : /* no input */
-        : /* no clobber */
-    );
-
+void software_interrupt_handler(int syscall_number, void *arg1, void *arg2, void *arg3, void *arg4) {
     int result = syscall_handler(syscall_number, arg1, arg2, arg3, arg4);
 
     asm volatile ("movl %0, %%eax" : : "r"(result));
