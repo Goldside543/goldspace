fe6c702f58f08216698b9860bd757b2c6ef40bfe
Goldside543 <goldside000@outlook.com>
2025-06-04 23:06:45 -0400

    Remove unnecessary curly braces

--- kernel/syscall_dispatcher.c
+++ kernel/syscall_dispatcher.c
@@ -16,9 +16,8 @@
 
 int syscall_handler(int syscall_number, void* arg1, void* arg2, void* arg3, void* arg4) {
     // Check if syscall_number is within valid range
-    if (syscall_number < 0 || syscall_number >= SYSCALL_TABLE_SIZE) {
+    if (syscall_number < 0 || syscall_number >= SYSCALL_TABLE_SIZE)
         return -1;
-    }
 
     // Get the function pointer from the syscall_table
     int (*handler)(void*, void*, void*, void*) = syscall_table[syscall_number];
