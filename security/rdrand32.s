026ead75587a632a04719922dbb18357765419f9
Goldside543 <goldside000@outlook.com>
2025-06-19 00:12:41 -0400

    Create rdrand32.s

---
+++ security/rdrand32.s
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+.global rdrand32
+
+rdrand32:
+    pushl %ebx
+    pushl %ecx
+
+    movl $01H, %eax
+    cpuid
+    
+    btl $30, %ecx
+
+    jnc not_set
+
+    rdrand %eax
+    popl %ebx
+    popl %ecx
+    ret
+    
+not_set:
+    xorl %eax, %eax
+    popl %ebx
+    popl %ecx
+    ret
