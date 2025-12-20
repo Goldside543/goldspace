ff48de841015632216c9fe70a7916149a7eda8cb
Goldside543 <goldside000@outlook.com>
2025-12-19 22:23:59 -0500

    Fix up the transition from kernel to user

--- kernel/ring3.s
+++ kernel/ring3.s
@@ -1,21 +1,30 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+.global user_stack
 .global jump_usermode
 .extern run_user_space
 
+.section .userland
+.balign 16                   # Align the following symbol to a 16-byte boundary
+user_stack:
+    .space 8192              # Allocate 8192 bytes of uninitialized space
+
+.section .text
 jump_usermode:
-    movl $0x3, %eax               # Load 3 into eax
-    orl $0x20, %eax               # Set the lower 2 bits for ring 3 (0x3 = 00000011b, 0x20 = 00100000b)
-    movl %eax, %ds                # Move the value into ds
-    movl %eax, %es                # Move the value into es
-    movl %eax, %fs                # Move the value into fs
-    movl %eax, %gs                # Move the value into gs
+    cli                          # Clear interrupts
+
+    movl $0x23, %ax              # Load 3 into eax
+    movl %ax, %ds                # Move the value into ds
+    movl %ax, %es                # Move the value into es
+    movl %ax, %fs                # Move the value into fs
+    movl %ax, %gs                # Move the value into gs
 
     # Set up the stack frame iret expects
-    movl %esp, %eax                # Move current esp into eax
+    movl $user_stack + 8192, %eax  # Move current esp into eax
     pushl $0x23                    # Push the data selector (4*8 | 3 = 0x23)
     pushl %eax                     # Push current esp
     pushfl                         # Push flags
+    orl $0x200, (%esp)             # Set IF in the saved flags
     pushl $0x1b                    # Push the code selector (3*8 | 3 = 0x1B)
     pushl $run_user_space          # Push the address of the function to return to
     iret
